8babe88e17f272f4d36cf38389f687c8bcfa803a
RonaldRis <rrris402@gmail.com>
2019-11-20 10:13:42 -0600

    Uploading PhoneApp.iOS Native

---
+++ PhoneApp.iOS/ViewController.designer.cs
@@ -0,0 +1,60 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace PhoneApp.iOS
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btnCall { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btnTranslate { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField PhoneNumberText { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel txtPhoneNumber { get; set; }
+
+        [Action ("BtnCall_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void BtnCall_TouchUpInside (UIKit.UIButton sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (btnCall != null) {
+                btnCall.Dispose ();
+                btnCall = null;
+            }
+
+            if (btnTranslate != null) {
+                btnTranslate.Dispose ();
+                btnTranslate = null;
+            }
+
+            if (PhoneNumberText != null) {
+                PhoneNumberText.Dispose ();
+                PhoneNumberText = null;
+            }
+
+            if (txtPhoneNumber != null) {
+                txtPhoneNumber.Dispose ();
+                txtPhoneNumber = null;
+            }
+        }
+    }
+}
